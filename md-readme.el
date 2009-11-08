78ccabc00a1f318e6234258975dd53e6ae1cf7f7
Thomas Kappler <thomas@linux-dmcx.site>
2009-11-08 13:51:55 +0100

    Recognize headers and the GPL disclaimer.

--- md-readme.el
+++ md-readme.el
@@ -30,16 +30,17 @@
 
 ;; Emacs Lisp files customarily have a header in a fairly standardized
 ;; format. md-readme extracts this header and re-formats it to
-;; Markdown. If you put your code on github, you can have this run
-;; from a git pre-commit hook so you automatically have an up-to-date
-;; README on github.
+;; Markdown. If you put your code on github, you could have this run
+;; automatically, for instance upon saving the file or from a git
+;; pre-commit hook, so you always have an up-to-date README on github.
 
 ;; It recognizes headings, the GPL license disclaimer which is
 ;; replaced by a shorter notice linking to the GNU project's license
 ;; website, lists, and normal paragraph. Lists are somewhat tricky to
 ;; recognize automatically.
 
-;;; Dependencies: none.
+;;; Dependencies:
+;; None.
 
 ;;; Installation:
 ;; (require 'md-readme) in your init file, possibly in an
@@ -49,19 +50,60 @@
 ;; 2009-11:    First release.
 
 ;;; Code:
+(progn
+  (set-buffer (mdr-put-header-in-temp-buffer))
+  (mdr-convert-header)
+)
 
 
-; go through line by line.
+(defun mdr-convert-header ()
+  (goto-char (point-min))
+  (mdr-find-and-replace-disclaimer)
+  (while (< (line-number-at-pos) (line-number-at-pos (point-max)))
+    (when (looking-at-p ";;")
+      (delete-char 2)
+      (cond ((looking-at-p ";")  ; heading
+	     (delete-char 1)
+	     (insert "#")
+	     (progn
+	       (end-of-line)
+	       (backward-char)
+	       (when (looking-at-p ":")
+		 (delete-char 1))))
+	    (())
+	    (t (delete-char 1)) ; whitespace
+	    )
+      
+      )
+    (forward-line 1)))
 
-; does not start with two ; and is not blank and "Code:" not yet
-; encountered? error.
+(defun mdr-put-header-in-temp-buffer ()
+  (let ((oldbuf (current-buffer))
+	(oldbuf-start (point-min))
+	(oldbuf-end-header (mdr-end-of-header)))
+    (save-excursion
+      (set-buffer (generate-new-buffer "md-readme-tmp.md"))
+      (insert-buffer-substring oldbuf oldbuf-start oldbuf-end-header)
+      (current-buffer))))
 
-; three ;? # heading. remove : at the end.
+(defun mdr-end-of-header ()
+  (save-excursion
+    (goto-char (point-min))
+    (while (or (looking-at-p "\n") (looking-at-p ";;"))
+      (forward-line 1))
+    (point)))
+
+(defun mdr-find-and-replace-disclaimer ()
+  (save-excursion
+    (goto-char (point-min))
+    (when (search-forward "This program is free software" nil t)
+      (let ((start-line (progn (beginning-of-line) (point)))
+      	    (end-line (search-forward
+      		       "If not, see <http://www.gnu.org/licenses/>."
+      		       nil t)))
+      	(delete-region start-line end-line)
+      	(insert "Licensed under the [GPL version 3](http://www.gnu.org/licenses/) or later.")))))
 
 ; list? heuristic:
 ;  - if next line not empty, line length + length of next word < 70.
 ;  - More than one line starts with Capital:
-
-; GPL notice? -> hard-coded text. Attention to version!
-
-; in any case: just remove the ";; ".
